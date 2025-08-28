b40034f840c5da96b13eb01d4529a431255ecf3a
jakimiszka <jarek.jakimicha@gmail.com>
2025-08-28 11:30:57 +0200

    prototype of random password

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const slider = document.querySelector(".slider");
 const style = document.querySelector('[data="test"]');
 const generate_button = document.querySelector(".grid__generator--button_generate");
+const slider_value = document.querySelector(".grid__generator--slider--input_value");
 
-slider.oninput = function() {
-    setDefault(slider.value);
-}
+const uppercase_check = document.querySelector("#uppercase");
+const lowercase_check = document.querySelector("#lowercase");
+const numbers_check = document.querySelector("#numbers");
+const symbols_check = document.querySelector("#symbols");
 
 function setDefault(x){
     Number(x) == 0 ?
-        style.innerHTML += ".slider::-webkit-slider-thumb {background: white; }" :
-        style.innerHTML = ".slider::-webkit-slider-thumb {background: var(--green-200); }"
+        style.innerHTML += ".slider::-webkit-slider-thumb {background: white;} .slider{background: var(--grey-850);}" :
+        style.innerHTML = ".slider::-webkit-slider-thumb {background: var(--white);} .slider{background: var(--green-200);}"
 }
 
 // prototype
@@ -17,17 +19,44 @@ function generatePassword() {
     const numbers = '0123456789';
     const letters = 'abcdefghijklmnopqrstuvwxyz';
     const symbols = '!@#$%&*';
-    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%&*";
-    const passwordLength = slider.value;
+    let passwordLength = Number(slider.value);
+    const   num = numbers_check.checked, 
+            lett = lowercase_check.checked, 
+            symb = symbols_check.checked;
     let password = "";
-    for (var i = 0; i <= passwordLength; i++) {
-        // get length basen od checked boxes
-            // get random number for each category
-        const randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars[randomNumber];
+    
+    for (var i = 0; i < passwordLength; i++) {
+        const randomNumber = Math.floor(Math.random() * letters.length);
+        if (num) {
+            const randomNumber = Math.floor(Math.random() * numbers.length);
+            password += numbers[randomNumber];
+            passwordLength -= 1;
+        } 
+        if (lett) {
+            const randomNumber = Math.floor(Math.random() * letters.length);
+            password += letters[randomNumber];
+            passwordLength -= 1;
+        } 
+        if (symb) {
+            const randomNumber = Math.floor(Math.random() * symbols.length);
+            password += symbols[randomNumber];
+            passwordLength -= 1;
+        }
+        password += letters[randomNumber];
+        //passwordLength -= 1;
     }
+    console.log(slider.value);
     console.log(password);
+    console.log(num, lett, symb);
 }
 
+console.log(uppercase_check.checked)
+
+slider.oninput = function() {
+    const sliderValue = slider.value;
+    setDefault(sliderValue);
+    slider_value.innerHTML = sliderValue;
+}
 generate_button.addEventListener("click", generatePassword);
+
 
