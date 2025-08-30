5665db6de52bf9934927ddf965cd4cf8998c8f83
jakimiszka <jarek.jakimicha@gmail.com>
2025-08-30 13:35:09 +0200

    done - clean up todo

--- js/script.js
+++ js/script.js
@@ -6,9 +6,12 @@ const uppercase_check = document.querySelector("#uppercase");
 const lowercase_check = document.querySelector("#lowercase");
 const numbers_check = document.querySelector("#numbers");
 const symbols_check = document.querySelector("#symbols");
+const strength_level = document.querySelector(".strength_level");
+const copy_button = document.querySelector('.copy_button');
+const password_value = document.querySelector(".grid__password--input_text");
 const numbers = '0123456789',
       letters = 'abcdefghijklmnopqrstuvwxyz',
-      symbols = '!@#$%&*-+_=\"\'<>?:;[]{}()\\/<>,.|^~';
+      symbols = '!@#$%&*+_=\"\'<>?:;[]{}()\\/<>,.|^~';
 const options = {
     'numbers': numbers,
     'letters': letters,
@@ -20,6 +23,12 @@ const strength = {
     '2': 'orange',
     '3': 'blue',
     '4': 'red'
+}
+const strengthLevel = {
+    '1': 'VERY WEAK',
+    '2': 'WEAK',
+    '3': 'MEDIUM',
+    '4': 'STRONG'
 }
 
 function getRandomChar(str) {
@@ -43,11 +52,7 @@ function recursivePassword(length, charSets, result = "") {
 function generatePassword(){
     let password = '';
     const passwordLength = Number(slider.value);
-    const charSets = [];
-    if (uppercase_check.checked) charSets.push(options.letters.toUpperCase());
-    if (lowercase_check.checked) charSets.push(options.letters);
-    if (numbers_check.checked) charSets.push(options.numbers);
-    if (symbols_check.checked) charSets.push(options.symbols);
+    const charSets = getcharSets();
 
     if (passwordLength === 0) return '';
     if (charSets.length === 0) {
@@ -74,38 +79,71 @@ function passwordStrength(password, charSetsLength){
     // default
     strengthLi.forEach((li) => {
         li.style.backgroundColor = 'var(--grey-850)';
-        li.style.borderColor = 'var(--white)';
+        li.style.borderColor = 'white';
     })
-    // options selected
-    const color = strength[charSetsLength];
-    for(let i=0; i< charSetsLength; i++){
-        const strengthElement = document.querySelector(`ul > li:nth-child(${i+1})`);
-        strengthElement.style.backgroundColor = color;
-        strengthElement.style.borderColor = color;
+
+    const valuePasswordStrength = (password, charSetsLength) =>{
+        const passwordLength = password.length;
+        if(passwordLength >= 8 && charSetsLength === 4){
+            return 4;
+        }else if(passwordLength >= 8 && charSetsLength === 3){
+            return 3;
+        }else if(passwordLength >= 10 && charSetsLength === 2){
+            return 2;
+        }else if(passwordLength >= 10 && charSetsLength === 1){
+            return 1;
+        }
     }
-
-    // cases
-        // 1. case len per easch set
-            // lowercase == 8 -> Strong
-            // uppercase == 8 -> Strong
-            // numbers == 10 -> medium
-            // symbols == 8 -> medium   
-        // 2. mixed sets (2, 3) and length
-            
-        // 3. all sets and length
-
-    // 1. min length 6
-        // all checked - VERY STRONG
+    const strengthValue = valuePasswordStrength(password, charSetsLength);
+    for(let i=0; i< strengthValue; i++){
+        const strengthElement = document.querySelector(`ul > li:nth-child(${i+1})`);
+        strengthElement.style.backgroundColor = strength[strengthValue];
+        strengthElement.style.borderColor = strength[strengthValue];
+    }
+    strengthLevel[strengthValue] ? strength_level.innerHTML = strengthLevel[strengthValue] : strength_level.innerHTML = '';
 }
+
+function checkIfButtonDisabled(){
+    const charSets = getcharSets();
+    if(Number(slider.value) > 0 && slider.value <= charSets.length || Number(slider.value) == 0 || charSets.length == 0){
+        generate_button.classList.add('disabled'); 
+        generate_button.disabled = true;
+    }else{
+        generate_button.disabled = false;
+        generate_button.classList.remove('disabled');
+    }
+}
+
+function getcharSets(){
+    const charSets = [];
+    if (uppercase_check.checked) charSets.push(options.letters.toUpperCase());
+    if (lowercase_check.checked) charSets.push(options.letters);
+    if (numbers_check.checked) charSets.push(options.numbers);
+    if (symbols_check.checked) charSets.push(options.symbols);
+
+    return charSets;
+}
+
+checkIfButtonDisabled();
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach((checkbox) => 
+    checkbox.addEventListener('change', () => {
+        checkIfButtonDisabled();
+}));
 
 slider.oninput = function() {
     const sliderValue = slider.value;
     slider_value.innerHTML = sliderValue;
+    checkIfButtonDisabled()
 }
+
 generate_button.addEventListener("click", () =>{
     const password = generatePassword();
-    console.log('password: ',password);
-    console.log('password len: ', password.length);
+    password_value.innerHTML = password;
 });
 
-
+copy_button.addEventListener('click', () => {
+    const pass = password_value.innerHTML;
+    navigator.clipboard.writeText(pass);
+}); 
